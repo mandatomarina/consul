4cddbc66277cbc535100cd70834c9b9d575ce506
Pedro Markun <pedro@markun.com.br>
2020-10-15 14:34:24 -0300

    mudanças

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,12 @@
 module Consul
   class Application < Rails::Application
+    config.i18n.default_locale = :"pt-BR"
+    available_locales = [
+      "pt-BR",
+      "es"]
+    config.i18n.available_locales = available_locales
+    config.i18n.fallbacks = {
+      "pt-BR" => "es",
+      }
   end
 end
